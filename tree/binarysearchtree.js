81603028a7144e79399ddc7426a03c2f00a7c44c
[xjylyh] <18001300327@163.com>
2018-05-10 17:58:12 +0800

    tree

--- tree/binarysearchtree.js
+++ tree/binarysearchtree.js
@@ -8,6 +8,10 @@
      max：返回树中最大的值/键。
      remove(key)：从树中移除某个键。
  */ 
+
+ //辅助方法开始
+ 
+ //向树中插入一个新的键
 let insertNode = function(node,newNode){
     if(newNode.key<node.key){
         if(node.left == null){
@@ -23,6 +27,34 @@ let insertNode = function(node,newNode){
         }
     }
 }
+//通过中序遍历方式遍历所有节点
+let inOrderTraverseNode = function(node,cb){
+    if(node!=null){
+        inOrderTraverseNode(node.left,cb);
+        cb(node.key);
+        inOrderTraverseNode(node.right,cb);
+    }
+}
+//通过先序遍历方式遍历所有节点
+let preOrderTraverseNode = function(node,cb){
+    if(node!=null){
+        cb(node.key);
+        preOrderTraverseNode(node.left,cb);
+        preOrderTraverseNode(node.right,cb);
+    }
+}
+//通过后序遍历方式遍历所有节点
+let postOrderTraverseNode = function(node,cb){
+    if(node!=null){
+        postOrderTraverseNode(node.left,cb);
+        postOrderTraverseNode(node.right,cb);
+        cb(node.key);
+    }
+}
+//辅助方法结束
+
+
+//二叉搜索树类
 function BinarySearchTree(){
     let Node = function(key){
         this.key = key;
@@ -40,6 +72,23 @@ function BinarySearchTree(){
             insertNode(this.root,newNode)
         }
     }
+
+    this.search = (key)=>{
+
+    }
+
+    //树的中序遍历
+    this.inOrderTraverse = (callback)=>{
+        inOrderTraverseNode(this.root,callback);
+    }
+    //树的前序遍历
+    this.preOrderTraverse = (callback)=>{
+        preOrderTraverseNode(this.root,callback);
+    }
+    //树的后序遍历
+    this.postOrderTraverse = (callback)=>{
+        postOrderTraverseNode(this.root,callback);
+    }
 }
 
 let tree = new BinarySearchTree();
@@ -47,6 +96,7 @@ tree.insert(11);
 tree.insert(7);
 tree.insert(15);
 tree.insert(5);
+tree.insert(6);
 tree.insert(3);
 tree.insert(9);
 tree.insert(8);
@@ -58,3 +108,6 @@ tree.insert(20);
 tree.insert(18);
 tree.insert(25); 
 console.log(tree.root);
+tree.postOrderTraverse(function(item){
+    console.log(item);
+})
